51426fc643022a52a5d17bf899681eca6b44d43b
stalechilibeans <73906828+stalechilibeans@users.noreply.github.com>
2024-12-09 15:56:30 -0500

    castle inside tweaks

--- src/game/object_helpers.c
+++ src/game/object_helpers.c
@@ -190,6 +190,7 @@ Gfx *geo_switch_area(s32 callContext, struct GraphNode *node) {
             if (sp20) {
                 gMarioCurrentRoom = sp20->room;
                 sp26 = sp20->room - 1;
+                // obj helpers
                 // gDebugInfoFlags|=DEBUG_INFO_FLAG_DPRINT; 
                 print_debug_top_down_objectinfo("areainfo %d", sp20->room);
 
