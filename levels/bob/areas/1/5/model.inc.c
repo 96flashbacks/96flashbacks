3a6997f59d795421f81120c12c29243d67d6102f
stalechilibeans <73906828+stalechilibeans@users.noreply.github.com>
2025-04-20 11:24:55 -0500

    fixed bob fences n stuff

--- levels/bob/areas/1/5/model.inc.c
+++ levels/bob/areas/1/5/model.inc.c
@@ -33,7 +33,35 @@ Vtx bobo_5_dl_mesh_vtx_0[32] = {
 	{{ {8093, 1936, -5982}, 0, {-18604, 1998}, {145, 0, 195, 255} }},
 };
 
-Gfx bobo_5_dl_mesh_tri_0[] = {
+Vtx bobo_5_dl_mesh_vtx_1[8] = {
+	{{ {-993, 1690, -2310}, 0, {22640, 1008}, {143, 0, 57, 255} }},
+	{{ {-2262, 1690, -4824}, 0, {156, 1008}, {143, 0, 57, 255} }},
+	{{ {-2262, 1075, -4824}, 0, {156, 1958}, {143, 0, 57, 255} }},
+	{{ {-993, 1075, -2310}, 0, {22640, 1958}, {143, 0, 57, 255} }},
+	{{ {-3128, 1690, -6537}, 0, {-11288, 1008}, {143, 0, 57, 255} }},
+	{{ {-3128, 1075, -6537}, 0, {-11288, 1958}, {143, 0, 57, 255} }},
+	{{ {-2436, 1075, -5167}, 0, {974, 1958}, {143, 0, 57, 255} }},
+	{{ {-2436, 1690, -5167}, 0, {974, 1008}, {143, 0, 57, 255} }},
+};
+
+// 0x0700A800 - 0x0700A848 -- bar
+static const Gfx bob_seg7_dl_0700A800[] = {
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, generic_09008800),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 32 * 32 - 1, CALC_DXT(32, G_IM_SIZ_16b_BYTES)),
+	gsSPVertex(bobo_5_dl_mesh_vtx_1 + 0, 8, 0),
+	gsSP1Triangle(0, 1, 2, 0),
+	gsSP1Triangle(0, 2, 3, 0),
+	gsSP1Triangle(4, 5, 6, 0),
+	gsSP1Triangle(7, 4, 6, 0),
+	gsSPEndDisplayList(),
+};
+
+// 0x0700A848 - 0x0700A920 -- fence
+static const Gfx bob_seg7_dl_0700A848[] = {
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, bob_seg7_texture_07000000),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 32 * 32 - 1, CALC_DXT(32, G_IM_SIZ_16b_BYTES)),
 	gsSPVertex(bobo_5_dl_mesh_vtx_0 + 0, 16, 0),
 	gsSP1Triangle(0, 1, 2, 0),
 	gsSP1Triangle(0, 3, 1, 0),
@@ -55,80 +83,30 @@ Gfx bobo_5_dl_mesh_tri_0[] = {
 	gsSPEndDisplayList(),
 };
 
-Vtx bobo_5_dl_mesh_vtx_1[8] = {
-	{{ {-993, 1690, -2310}, 0, {22640, 1008}, {143, 0, 57, 255} }},
-	{{ {-2262, 1690, -4824}, 0, {156, 1008}, {143, 0, 57, 255} }},
-	{{ {-2262, 1075, -4824}, 0, {156, 1958}, {143, 0, 57, 255} }},
-	{{ {-993, 1075, -2310}, 0, {22640, 1958}, {143, 0, 57, 255} }},
-	{{ {-3128, 1690, -6537}, 0, {-11288, 1008}, {143, 0, 57, 255} }},
-	{{ {-3128, 1075, -6537}, 0, {-11288, 1958}, {143, 0, 57, 255} }},
-	{{ {-2436, 1075, -5167}, 0, {974, 1958}, {143, 0, 57, 255} }},
-	{{ {-2436, 1690, -5167}, 0, {974, 1008}, {143, 0, 57, 255} }},
+// 0x0700A920 - 0x0700A9E0
+const Gfx bobo_5_dl_mesh[] = {
+    gsDPPipeSync(),
+    gsDPSetCycleType(G_CYC_2CYCLE),
+    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_TEX_EDGE2),
+    gsDPSetDepthSource(G_ZS_PIXEL),
+    gsDPSetFogColor(160, 160, 160, 255),
+    gsSPFogPosition(980, 1000),
+    gsSPSetGeometryMode(G_FOG),
+    gsDPSetCombineMode(G_CC_DECALRGBA, G_CC_PASS2),
+    gsSPClearGeometryMode(G_LIGHTING | G_CULL_BACK),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, 5, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, 5, G_TX_NOLOD),
+    gsDPSetTileSize(0, 0, 0, (32 - 1) << G_TEXTURE_IMAGE_FRAC, (32 - 1) << G_TEXTURE_IMAGE_FRAC),
+    gsSPDisplayList(bob_seg7_dl_0700A800),
+    gsSPDisplayList(bob_seg7_dl_0700A848),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_OFF),
+    gsDPPipeSync(),
+    gsDPSetCycleType(G_CYC_1CYCLE),
+    gsDPSetRenderMode(G_RM_AA_ZB_TEX_EDGE, G_RM_NOOP2),
+    gsSPClearGeometryMode(G_FOG),
+    gsDPSetCombineMode(G_CC_SHADE, G_CC_SHADE),
+    gsSPSetGeometryMode(G_LIGHTING | G_CULL_BACK),
+    gsSPEndDisplayList(),
 };
-
-Gfx bobo_5_dl_mesh_tri_1[] = {
-	gsSPVertex(bobo_5_dl_mesh_vtx_1 + 0, 8, 0),
-	gsSP1Triangle(0, 1, 2, 0),
-	gsSP1Triangle(0, 2, 3, 0),
-	gsSP1Triangle(4, 5, 6, 0),
-	gsSP1Triangle(7, 4, 6, 0),
-	gsSPEndDisplayList(),
-};
-
-Gfx mat_bobo_fence[] = {
-	gsSPClearGeometryMode(G_CULL_BACK),
-	gsDPPipeSync(),
-	gsDPSetCombineLERP(0, 0, 0, TEXEL0, 0, 0, 0, TEXEL0, 0, 0, 0, TEXEL0, 0, 0, 0, TEXEL0),
-	gsDPSetAlphaDither(G_AD_NOISE),
-	gsSPTexture(65535, 65535, 0, 0, 1),
-	gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b_LOAD_BLOCK, 1, bob_seg7_texture_07000000),
-	gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b_LOAD_BLOCK, 0, 0, 7, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0),
-	gsDPLoadBlock(7, 0, 0, 1023, 256),
-	gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 5, 0, G_TX_WRAP | G_TX_NOMIRROR, 5, 0),
-	gsDPSetTileSize(0, 0, 0, 124, 124),
-	gsSPEndDisplayList(),
-};
-
-Gfx mat_revert_bobo_fence[] = {
-	gsSPSetGeometryMode(G_CULL_BACK),
-	gsDPPipeSync(),
-	gsDPSetAlphaDither(G_AD_DISABLE),
-	gsSPEndDisplayList(),
-};
-
-Gfx mat_bobo_bar[] = {
-	gsSPClearGeometryMode(G_CULL_BACK),
-	gsDPPipeSync(),
-	gsDPSetCombineLERP(0, 0, 0, TEXEL0, 0, 0, 0, TEXEL0, 0, 0, 0, TEXEL0, 0, 0, 0, TEXEL0),
-	gsDPSetAlphaDither(G_AD_NOISE),
-	gsSPTexture(65535, 65535, 0, 0, 1),
-	gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b_LOAD_BLOCK, 1, generic_09008800),
-	gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b_LOAD_BLOCK, 0, 0, 7, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0),
-	gsDPLoadBlock(7, 0, 0, 1023, 256),
-	gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 5, 0, G_TX_WRAP | G_TX_NOMIRROR, 5, 0),
-	gsDPSetTileSize(0, 0, 0, 124, 124),
-	gsSPEndDisplayList(),
-};
-
-Gfx mat_revert_bobo_bar[] = {
-	gsSPSetGeometryMode(G_CULL_BACK),
-	gsDPPipeSync(),
-	gsDPSetAlphaDither(G_AD_DISABLE),
-	gsSPEndDisplayList(),
-};
-
-Gfx bobo_5_dl_mesh[] = {
-	gsSPDisplayList(mat_bobo_fence),
-	gsSPDisplayList(bobo_5_dl_mesh_tri_0),
-	gsSPDisplayList(mat_revert_bobo_fence),
-	gsSPDisplayList(mat_bobo_bar),
-	gsSPDisplayList(bobo_5_dl_mesh_tri_1),
-	gsSPDisplayList(mat_revert_bobo_bar),
-	gsDPPipeSync(),
-	gsSPSetGeometryMode(G_LIGHTING),
-	gsSPClearGeometryMode(G_TEXTURE_GEN),
-	gsDPSetCombineLERP(0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT, 0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT),
-	gsSPTexture(65535, 65535, 0, 0, 0),
-	gsSPEndDisplayList(),
-};
-
